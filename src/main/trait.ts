9e52a7741249d4f5902d3019edc7f02c4b4629bf
Matthew Adams <matthew@matthewadams.me>
2018-10-23 12:48:51 -0500

    compiling but test failing

--- src/main/trait.ts
+++ src/main/trait.ts
@@ -7,4 +7,19 @@ type ClassInstanceType<T> = T extends new (...args: any[]) => infer R
 type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
 type MergeClassTypes<T extends Array<Class<any>>> = UnionToIntersection<ClassInstanceType<T[number]>>
 
-export default function traits<T extends Array<Class<any>>> (constructors: T): Constructor<MergeClassTypes<T>>
+export default function traits<T extends Array<Class<any>>> (constructors: T): Constructor<MergeClassTypes<T>> {
+  const cls = class {
+    state = {}
+
+    constructor () {
+      constructors.forEach((c: any) => {
+        c.apply(this)
+      })
+    }
+  }
+  constructors.forEach((c: any) => {
+    Object.assign(cls.prototype, c.prototype)
+  })
+  return cls as any
+}
+/* End Implementation */
