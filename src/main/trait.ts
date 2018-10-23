f6b155a17ad702bcdd972a6ee5f8cf3ec9234124
Matthew Adams <matthew@matthewadams.me>
2018-10-23 13:09:12 -0500

    try to use Reflect.construct

--- src/main/trait.ts
+++ src/main/trait.ts
@@ -1,10 +1,11 @@
 type Constructor<T = {}> = new (...args: any[]) => T
+
 type Class<T> = Function & { prototype: T }
 
-type ClassInstanceType<T> = T extends new (...args: any[]) => infer R
-  ? R
-  : T extends { prototype: infer R } ? R : any;
+type ClassInstanceType<T> = T extends new (...args: any[]) => infer R  ? R  : T extends { prototype: infer R } ? R : any;
+
 type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+
 type MergeClassTypes<T extends Array<Class<any>>> = UnionToIntersection<ClassInstanceType<T[number]>>
 
 export default function traits<T extends Array<Class<any>>> (constructors: T): Constructor<MergeClassTypes<T>> {
@@ -13,7 +14,7 @@ export default function traits<T extends Array<Class<any>>> (constructors: T): C
 
     constructor () {
       constructors.forEach((c: any) => {
-        c.apply(this)
+        Reflect.construct(c, [], this)
       })
     }
   }
