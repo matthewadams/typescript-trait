aee6972cb895d115b3718defd8ed2a753eec173c
Matthew Adams <matthew@matthewadams.me>
2018-10-23 12:27:49 -0500

    use traits.js from @CyrusNajmabadi

--- src/main/trait.ts
+++ src/main/trait.ts
@@ -1,25 +1,10 @@
-/* implementation */
 type Constructor<T = {}> = new (...args: any[]) => T
+type Class<T> = Function & { prototype: T }
 
-/* turns A | B | C into A & B & C */
+type ClassInstanceType<T> = T extends new (...args: any[]) => infer R
+  ? R
+  : T extends { prototype: infer R } ? R : any;
 type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+type MergeClassTypes<T extends Array<Class<any>>> = UnionToIntersection<ClassInstanceType<T[number]>>
 
-/* merges constructor types - self explanitory */
-type MergeConstructorTypes<T extends Array<Constructor<any>>> = UnionToIntersection<InstanceType<T[number]>>
-
-export default function traits<T extends Array<Constructor<any>>> (constructors: T): Constructor<MergeConstructorTypes<T>> {
-  const cls = class {
-    state = {}
-
-    constructor () {
-      constructors.forEach((c: any) => {
-        c.apply(this)
-      })
-    }
-  }
-  constructors.forEach((c: any) => {
-    Object.assign(cls.prototype, c.prototype)
-  })
-  return cls as any
-}
-/* End Implementation */
+export default function traits<T extends Array<Class<any>>> (constructors: T): Constructor<MergeClassTypes<T>>
