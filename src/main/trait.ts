b0b79d0a3c1a01f0a3b19ac45925c623039a0150
Matthew Adams <matthew@matthewadams.me>
2018-10-24 10:04:58 -0500

    trying to go in a different direction

--- src/main/trait.ts
+++ src/main/trait.ts
@@ -1,37 +1,25 @@
-type Constructor<T = {}> = new (...args: any[]) => T
+// type ClassInstanceType<T> = T extends new (...args: any[]) => infer R ? R : T extends { prototype: infer R } ? R : any
+//
+// type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+//
+// type MergeClassTypes<T extends Array<Class<any>>> = UnionToIntersection<ClassInstanceType<T[number]>>
 
-type Class<T> = Function & { prototype: T }
+export type Constructor<T = {}> = new (...args: any[]) => T
 
-type ClassInstanceType<T> = T extends new (...args: any[]) => infer R ? R : T extends { prototype: infer R } ? R : any
+export type Class<T> = Function & { prototype: T }
 
-type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+export type Trait<T,SUP = {}> = (superclass?: Constructor<SUP>) => Class<T & SUP>
 
-type MergeClassTypes<T extends Array<Class<any>>> = UnionToIntersection<ClassInstanceType<T[number]>>
+export const superclass = <S = any>(s?: Constructor<S>) => new TraitBuilder(s)
 
-export default function traits<T extends Array<Class<any>>> (constructors: T): Constructor<MergeClassTypes<T>> {
-  const cls = class {
-    constructor () {
-      constructors.forEach((c: any) => {
-        const tmp = Reflect.construct(c, [], new.target)
-        let keys: string[] = []
-        for (let key of Object.keys(tmp)) {
-          const desc = Object.getOwnPropertyDescriptor(tmp, key)
-          if (desc !== undefined) {
-            Object.defineProperty(this, key, desc)
-          }
-          keys.push(key)
-        }
-      })
-    }
+export class TraitBuilder<S = any> {
+  superclass: Constructor<S>
+
+  constructor (superclass?: Constructor<S>) {
+    this.superclass = superclass || (class {} as any)
   }
-  constructors.forEach((c: any) => {
-    for (let key of Object.keys(c.prototype)) {
-      const desc = Object.getOwnPropertyDescriptor(c.prototype, key)
-      if (desc !== undefined) {
-        Object.defineProperty(cls.prototype, key, desc)
-      }
-    }
-  })
-  return cls as any
+
+  expressing <T,U> (t: Trait<T,U>): Class<T & U> {
+    return t(this.superclass)
+  }
 }
-/* End Implementation */
