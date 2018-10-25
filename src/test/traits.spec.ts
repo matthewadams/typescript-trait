3ebd1634e9b7b48773b378f19ed358953f03e8a3
Matthew Adams <matthew@matthewadams.me>
2018-10-25 08:02:26 -0500

    broken wip using mutrait in anticipation of mutrait declaration file

--- src/test/traits.spec.ts
+++ src/test/traits.spec.ts
@@ -1,65 +1,40 @@
 import { expect } from 'chai'
 
-import traits from '../main/trait'
+import { Trait, superclass, trait, traits } from 'mutrait'
 
 describe('traits', () => {
   it('multiple supertraits with correct overrides', () => {
-    class /* trait */ Supertrait1 {
+    const Supertrait1 = Trait(s => class extends s {
       bleep () { return 'bleep from Supertrait1' }
       foo () { return 'foo from Supertrait1' }
       bar () { return 'bar from Supertrait1' }
       snafu () { return 'snafu from Supertrait1' }
-      get blargy () { return 'blargy from Supertrait1' }
-    }
+    })
 
-    class /* trait */ Supertrait2 extends Supertrait1 {
+    const Supertrait2 = Trait(s => class extends s {
       foo () { return 'foo from Supertrait2' }
       bar () { return 'bar from Supertrait2' }
       snafu () { return 'snafu from Supertrait2' }
-    }
+    })
 
-    class /* trait */ Subtrait extends Supertrait2 {
-      bar () { return 'bar from Subtrait' }
-      snafu () { return 'snafu from Subtrait' }
-    }
+    const Subtrait = Trait(s =>
+      class extends superclass(s).expressing(Supertrait1, Supertrait2) {
+        bar () { return 'bar from Subtrait' }
+        snafu () { return 'snafu from Subtrait' }
+      })
 
-    abstract class /* trait */ AnotherTrait {
-      _more: string = 'more from AnotherTrait'
-      get blargy () { return 'blargy from AnotherTrait' }
-      get another () { return 'another from AnotherTrait' }
-      get more () { return this._more }
-      set more (value: string) {
-        this._more = this.checkMore(value)
-      }
-      abstract checkMore (value: string): string
-    }
-
-    class C extends traits([Subtrait, AnotherTrait]) /* expresses Subtrait, AnotherTrait */ {
+    class C extends trait(Subtrait) {
       snafu () { return 'snafu from C' }
-      get another () { return 'another from C' }
-      checkMore (value: string): string {
-        if (!value) throw new Error('no more given')
-        return value
-      }
     }
 
     const c = new C()
-
     expect(c.bleep()).to.equal('bleep from Supertrait1')
     expect(c.foo()).to.equal('foo from Supertrait2')
     expect(c.bar()).to.equal('bar from Subtrait')
     expect(c.snafu()).to.equal('snafu from C')
-    expect(c.another).to.equal('another from C')
-    expect(c.blargy).to.equal('blargy from AnotherTrait')
-    expect(c.more).to.equal('more from AnotherTrait')
-    expect(() => {
-      c.more = ''
-    }).to.throw()
-
-    expect(c).to.be.instanceOf(C)
-    expect(c).to.be.instanceOf(Subtrait)
-    expect(c).to.be.instanceOf(Supertrait2)
-    expect(c).to.be.instanceOf(Supertrait1)
-    expect(c).to.be.instanceOf(AnotherTrait)
+    expect(c instanceof C).to.be.true
+    expect(c instanceof Subtrait).to.be.true
+    expect(c instanceof Supertrait2).to.be.true
+    expect(c instanceof Supertrait1).to.be.true
   })
 })
